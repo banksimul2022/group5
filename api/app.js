c611ea041159de29f4c17124333517b2405840fd
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-04-01 14:45:44 +0300

    Routet ja app.js tehty aluille

--- api/app.js
+++ api/app.js
@@ -2,12 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var AsiakasRouter = require('./routes/Asiakas');
+var KorttiRouter = require('./routes/Kortti');
+var TilitapahtumatRouter = require('./routes/Tilitapahtumat');
+var CreditTiliRouter = require('./routes/CreditTili');
+var DebitTiliRouter = require('./routes/DebitTili');
+var Asiakas_has_CreditTiliRouter = require('./routes/Asikas_has_CreditTili');
+var Asiakas_has_DebitTiliRouter = require('./routes/Asiakas_has_DebitTili');
+
 
 var app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +26,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/Asiakas', AsiakasRouter);
+app.use('/Kortti', KorttiRouter);
+app.use('/Tilitapahtumat', TilitapahtumatRouter);
+app.use('/CreditTili', CreditTiliRouter);
+app.use('/DebitTili', DebitTiliRouter);
+app.use('/Asiakas_has_CreditTili', Asiakas_has_CreditTiliRouter);
+app.use('/Asiakas_has_DebitTili', Asiakas_has_DebitTiliRouter);
 
 module.exports = app;
---
+++ api/routes/Asiakas_has_debitTili.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const Asikas_has_DebitTili = require('../models/Asikas_has_DebitTili_model');
+
+router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    Asikas_has_DebitTili.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  } else {
+    Asikas_has_DebitTili.getAll(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+  Asikas_has_DebitTili.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  Asikas_has_DebitTili.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  Asikas_has_DebitTili.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
