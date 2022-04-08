dbad8786d62cdd2797e8b2a26263b253766b62fb
VeikkaK <kursulaveikka@gmail.com>
2022-04-08 16:30:32 +0300

    tarpeellisia luokkia ja vahva kooste rfiddll ja exen välille

--- bankautomat/RFID_DLL/rfid_dll.h
+++ bankautomat/RFID_DLL/rfid_dll.h
@@ -3,10 +3,26 @@
 
 #include "RFID_DLL_global.h"
 
-class RFID_DLL_EXPORT RFID_DLL
+#include <QObject>
+#include <QDebug>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+class RFID_DLL_EXPORT RFID_DLL: public QObject
 {
+    Q_OBJECT
+
 public:
-    RFID_DLL();
+    RFID_DLL(QObject * parent = nullptr);
+   ~RFID_DLL();
+    void printti();
+
+signals:
+
+public slots:
+
+private:
+
 };
 
 #endif // RFID_DLL_H
