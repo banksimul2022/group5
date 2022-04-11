ab3cbc5046ccb5a91e642461c33e7a421fa08997
VeikkaK <kursulaveikka@gmail.com>
2022-04-11 17:47:01 +0300

    rfid dll edistystä

--- bankautomat/mainwindow.cpp
+++ bankautomat/mainwindow.cpp
@@ -7,13 +7,9 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     pRFID_DLL = new RFID_DLL;
-<<<<<<< HEAD
     pRFID_DLL->luekortinid();
-=======
-    pRFID_DLL->printti();
     pPinkoodi_dll = new Pinkoodi_dll;
 
->>>>>>> 61422029d4499ea6ecb70d7d673bc09aedc33b22
 }
 
 MainWindow::~MainWindow()
