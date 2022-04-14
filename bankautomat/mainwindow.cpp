f094f1a17df8e4d5ef8e13bc5c41fbdcf0f8000f
VeikkaK <kursulaveikka@gmail.com>
2022-04-14 17:29:32 +0300

    koodia

--- bankautomat/mainwindow.cpp
+++ bankautomat/mainwindow.cpp
@@ -10,12 +10,6 @@ MainWindow::MainWindow(QWidget *parent)
     pPinkoodi_dll = new Pinkoodi_dll;
 
     pRFID_DLL->luekortinid();
-    connect(pRFID_DLL,SIGNAL(laheta(QByteArray)),
-            this, SLOT(RFID_slot(QByteArray)));
-
-    connect(pPinkoodi_dll, SIGNAL(pinkoodi_signal(QString)),
-            this, SLOT(pinkoodi_slot(QString)));
-
 }
 
 MainWindow::~MainWindow()
@@ -28,26 +22,8 @@ MainWindow::~MainWindow()
     pPinkoodi_dll = nullptr;
 
 }
-
-<<<<<<< HEAD
 void MainWindow::on_HYVAKSY_clicked()
 {
     pPinkoodi_dll->show();
 }
-=======
-void MainWindow::RFID_slot(QByteArray)
-{
-    pPinkoodi_dll->show();
-}
 
-void MainWindow::pinkoodi_slot(QString pinkoodi)
-{
-    qDebug()<<"pinkoodi exessä: " + pinkoodi;
-}
-
-void MainWindow::on_pushButton_clicked()
-{
-    pPinkoodi_dll->show();
-}
-
->>>>>>> 25e5b889254ed63f4e8f5602babe973b8de6a929
