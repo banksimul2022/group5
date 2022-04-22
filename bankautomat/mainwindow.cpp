517f6afa8592bcbfda7a3abd814c78f54540d2d4
VeikkaK <kursulaveikka@gmail.com>
2022-04-22 14:24:57 +0300

    timer alustettu

--- bankautomat/mainwindow.cpp
+++ bankautomat/mainwindow.cpp
@@ -8,8 +8,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     pRFID_DLL = new RFID_DLL;
     pPinkoodi_dll = new Pinkoodi_dll;
+    timer = new QTimer;
 
     pRFID_DLL->luekortinid();
+    startTimer();
+
 
     connect(pRFID_DLL,SIGNAL(laheta(QByteArray)),
                 this, SLOT(RFID_slot(QByteArray)));
@@ -26,6 +29,8 @@ MainWindow::~MainWindow()
     pRFID_DLL = nullptr;
     delete pPinkoodi_dll;
     pPinkoodi_dll = nullptr;
+    delete timer;
+    timer = nullptr;
 
 }
 
@@ -43,3 +48,10 @@ void MainWindow::pinkoodi_slot(QString pinkoodi)
 {
     qDebug()<<"pinkoodi exessä: " + pinkoodi;
 }
+
+void MainWindow::startTimer()
+{
+    qDebug() << "start timer 10sec";
+    timer->start(10000);
+}
+
