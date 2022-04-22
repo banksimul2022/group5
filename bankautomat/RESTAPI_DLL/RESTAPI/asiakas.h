512a99d570ba2f9c2e74513a02e6bcac7ee3ea82
Hakis1 <t1hajo02@students.oamk.fi>
2022-04-22 14:09:53 +0300

    lisäyksiä restapidll

---
+++ bankautomat/RESTAPI_DLL/RESTAPI/asiakas.h
@@ -0,0 +1,11 @@
+#ifndef ASIAKAS_H
+#define ASIAKAS_H
+
+
+class asiakas
+{
+public:
+    asiakas();
+};
+
+#endif // ASIAKAS_H
---
+++ bankautomat/RESTAPI_DLL/RESTAPI/myurl.h
@@ -0,0 +1,11 @@
+#ifndef MYURL_H
+#define MYURL_H
+
+
+class MyUrl
+{
+public:
+    MyUrl();
+};
+
+#endif // MYURL_H
