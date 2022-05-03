5ef900285ecd98ac57f8c51875093acec4377578
Hakis1 <t1hajo02@students.oamk.fi>
2022-05-03 16:29:52 +0300

    kkonaukko22

--- bankautomat/RESTAPI_DLL/RESTAPI/build/debug/moc_restapi.cpp
+++ bankautomat/RESTAPI_DLL/RESTAPI/build/debug/moc_restapi.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_RESTAPI_t {
-    QByteArrayData data[9];
-    char stringdata0[93];
+    QByteArrayData data[12];
+    char stringdata0[129];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,15 +37,19 @@ QT_MOC_LITERAL(1, 8, 12), // "login_signal"
 QT_MOC_LITERAL(2, 21, 0), // ""
 QT_MOC_LITERAL(3, 22, 13), // "asiakasSignal"
 QT_MOC_LITERAL(4, 36, 9), // "nimiToExe"
-QT_MOC_LITERAL(5, 46, 10), // "login_slot"
-QT_MOC_LITERAL(6, 57, 14), // "getasiakasSlot"
-QT_MOC_LITERAL(7, 72, 14), // "QNetworkReply*"
-QT_MOC_LITERAL(8, 87, 5) // "reply"
+QT_MOC_LITERAL(5, 46, 11), // "debitSignal"
+QT_MOC_LITERAL(6, 58, 10), // "saldoToExe"
+QT_MOC_LITERAL(7, 69, 10), // "login_slot"
+QT_MOC_LITERAL(8, 80, 14), // "getasiakasSlot"
+QT_MOC_LITERAL(9, 95, 14), // "QNetworkReply*"
+QT_MOC_LITERAL(10, 110, 5), // "reply"
+QT_MOC_LITERAL(11, 116, 12) // "getSaldoSlot"
 
     },
     "RESTAPI\0login_signal\0\0asiakasSignal\0"
-    "nimiToExe\0login_slot\0getasiakasSlot\0"
-    "QNetworkReply*\0reply"
+    "nimiToExe\0debitSignal\0saldoToExe\0"
+    "login_slot\0getasiakasSlot\0QNetworkReply*\0"
+    "reply\0getSaldoSlot"
 };
 #undef QT_MOC_LITERAL
 
@@ -55,30 +59,36 @@ static const uint qt_meta_data_RESTAPI[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       3,       // signalCount
+       5,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   39,    2, 0x06 /* Public */,
-       3,    1,   42,    2, 0x06 /* Public */,
-       4,    2,   45,    2, 0x06 /* Public */,
+       1,    1,   54,    2, 0x06 /* Public */,
+       3,    1,   57,    2, 0x06 /* Public */,
+       4,    2,   60,    2, 0x06 /* Public */,
+       5,    1,   65,    2, 0x06 /* Public */,
+       6,    1,   68,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       5,    1,   50,    2, 0x08 /* Private */,
-       6,    1,   53,    2, 0x08 /* Private */,
+       7,    1,   71,    2, 0x08 /* Private */,
+       8,    1,   74,    2, 0x08 /* Private */,
+      11,    1,   77,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QByteArray,    2,
     QMetaType::Void, QMetaType::QString,    2,
     QMetaType::Void, QMetaType::QString, QMetaType::QString,    2,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QByteArray,    2,
-    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, 0x80000000 | 9,   10,
+    QMetaType::Void, 0x80000000 | 9,   10,
 
        0        // eod
 };
@@ -92,14 +102,24 @@ void RESTAPI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         case 0: _t->login_signal((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
         case 1: _t->asiakasSignal((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 2: _t->nimiToExe((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
-        case 3: _t->login_slot((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
-        case 4: _t->getasiakasSlot((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        case 3: _t->debitSignal((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 4: _t->saldoToExe((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 5: _t->login_slot((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
+        case 6: _t->getasiakasSlot((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        case 7: _t->getSaldoSlot((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         switch (_id) {
         default: *reinterpret_cast<int*>(_a[0]) = -1; break;
-        case 4:
+        case 6:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QNetworkReply* >(); break;
+            }
+            break;
+        case 7:
             switch (*reinterpret_cast<int*>(_a[1])) {
             default: *reinterpret_cast<int*>(_a[0]) = -1; break;
             case 0:
@@ -127,6 +147,20 @@ void RESTAPI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
             using _t = void (RESTAPI::*)(QString , QString );
             if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&RESTAPI::nimiToExe)) {
                 *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (RESTAPI::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&RESTAPI::debitSignal)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (RESTAPI::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&RESTAPI::saldoToExe)) {
+                *result = 4;
                 return;
             }
         }
@@ -162,13 +196,13 @@ int RESTAPI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 8;
     }
     return _id;
 }
@@ -192,6 +226,20 @@ void RESTAPI::nimiToExe(QString _t1, QString _t2)
 {
     void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))) };
     QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void RESTAPI::debitSignal(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+
+// SIGNAL 4
+void RESTAPI::saldoToExe(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
