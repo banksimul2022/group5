f1838c908b1c566dcdbf715e92ff1618a3774dbe
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-05-03 16:47:52 +0300

    pls auttakaa

--- bankautomat/RESTAPI_DLL/RESTAPI/restapi.h
+++ bankautomat/RESTAPI_DLL/RESTAPI/restapi.h
@@ -20,8 +20,9 @@ public:
 
     void getAsiakas(QString tunnus);
     void getCredit(QString tilinnumero);
+    void getDebit(QString tilinnumero);
 
-    void getSaldo(QString tunnus);
+    QString getSaldo();
     void startSaldo();
 
     QString getTilitapahtumat();
@@ -32,33 +33,24 @@ public:
 private slots:
     void login_slot(QByteArray);
     void getasiakasSlot(QNetworkReply *reply);
-<<<<<<< HEAD
     void getcreditSlot (QNetworkReply *reply);
-=======
-    void getSaldoSlot(QNetworkReply *reply);
->>>>>>> 4dbcf35420d3d770701ec897e020384ad2174682
+    void getdebitSlot (QNetworkReply *reply);
 
 
 signals:
     void login_signal(QByteArray);
     void creditSignal(QString);
+    void debitSignal(QString);
     void asiakasSignal(QString);
     void nimiToExe(QString,QString);
-<<<<<<< HEAD
     void velkaToExe(QString);
-=======
-    void debitSignal(QString);
     void saldoToExe(QString);
->>>>>>> 4dbcf35420d3d770701ec897e020384ad2174682
 
 private:
 
         QNetworkAccessManager * asiakasManager;
-<<<<<<< HEAD
         QNetworkAccessManager * creditManager;
-=======
         QNetworkAccessManager * debitManager;
->>>>>>> 4dbcf35420d3d770701ec897e020384ad2174682
         QNetworkReply *reply;
         QByteArray response_data;
         Login *objectLogin;
